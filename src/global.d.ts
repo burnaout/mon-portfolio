c528fd17244f3644385be391d136c85d421d0ed7
Gabin Mercier <gabin@MacBook-Air-de-Gabin.local>
2026-05-10 01:05:20 +0200

    Description de ce que tu as changé

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+// Déclarations pour les modules CSS
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "@once-ui-system/core/css/styles.css";
+declare module "@once-ui-system/core/css/tokens.css";
+declare module "@/resources/custom.css";
